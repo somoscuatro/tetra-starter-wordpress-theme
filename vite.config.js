c8bb358976c82398851eb05750f49dae7c1eb956
Luca Pipolo <luca@somoscuatro.es>
2025-12-02 17:51:50 +0100

    build: switch from Laravel Mix to Vite

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+	root: '',
+	base: '',
+	server: {
+		https: {
+			key: '/etc/ssl/cert-key.pem',
+			cert: '/etc/ssl/cert.pem',
+		},
+		host: '0.0.0.0',
+		port: 5173,
+		strictPort: true,
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	build: {
+		manifest: true,
+		outDir: 'dist',
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, 'assets/scripts/main.ts'),
+				styles: path.resolve(__dirname, 'assets/styles/main.css'),
+				fonts: path.resolve(__dirname, 'assets/styles/fonts.css'),
+			},
+		},
+	},
+});
